b548ad9fecbf66afb9df2dac8df50627a1fb3211
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2021-12-23 14:20:20 +0200

    Add example for CardYield response

---
+++ WebService/API/Swagger/Example/QueriesController/CardYieldEndpointExample.cs
@@ -0,0 +1,26 @@
+﻿using Swashbuckle.AspNetCore.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebService.API.Controllers.Models;
+
+namespace WebService.API.Swagger.Example.QueriesController
+{
+    public class CardYieldRequestExample : IExamplesProvider<object>
+    {
+        public object GetExamples()
+        {
+            return new CardYieldModel() { Catalog = "X56868", StartDate = DateTime.Now, EndDate = DateTime.Now };
+        }
+    }
+    public class CardYieldResponseExample : IExamplesProvider<object>
+    {
+        public object GetExamples()
+        {
+            return new List<object> { new {Catalog= "X56868", CardName= "OA_HF", SuccessRatio=93.12 },
+            new {Catalog= "X56868", CardName= "OP_KLF", SuccessRatio=95.2 },
+            new {Catalog= "X56868", CardName= "OA_ASDF", SuccessRatio=89.2 }};
+        }
+    }
+}
