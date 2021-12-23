9e224a3b39b14cc7d1a21aa6598c59bb5b26ae6e
Zoe-Ellins <zoee@post.bgu.ac.il>
2021-12-23 20:20:45 +0200

    Update Query Result empty records case

--- WebService/Domain/DataAccess/LogsAndTestsRepository.cs
+++ WebService/Domain/DataAccess/LogsAndTestsRepository.cs
@@ -67,23 +67,29 @@ namespace WebService.Domain.DataAccess
                 await connection.OpenAsync();
                 var sqlCommand =
                 @"
-                SELECT T1.Catalog, T1.CardName,  CAST(((IsNull(SuccessTests, 0) * 100.0) / (IsNull(SuccessTests, 0) + IsNull(FailedTests, 0))) AS FLOAT) AS SuccessRatio
+                SELECT COALESCE (T1.Catalog,T2.Catalog) as Catalog, COALESCE(T1.CardName, T2.CardName) as CardName, CAST(((IsNull(SuccessTests, 0) * 100.0) / (IsNull(SuccessTests, 0) + IsNull(FailedTests, 0))) AS FLOAT) AS SuccessRatio
                 FROM (
 	                (SELECT Catalog, CardName, COUNT(*) as SuccessTests
-                    From Logs
-                    WHERE Catalog=@Catalog AND Logs.Date between @StartDate and @EndDate AND FinalResult = 'PASS'
-                    GROUP BY Catalog, CardName 
-                    ) as T1 
-                LEFT JOIN
-                    (SELECT Catalog, CardName, COUNT(*) as FailedTests
-                    From Logs
-                    WHERE Catalog=@Catalog AND Logs.Date between @StartDate and @EndDate AND FinalResult = 'FAIL'
-                    GROUP BY Catalog, CardName
-                    ) as T2
-	            ON T1.CardName = T2.CardName
+                From Logs
+                WHERE Catalog=@Catalog AND Logs.Date between @StartDate and @EndDate AND FinalResult = 'PASS'
+                GROUP BY Catalog, CardName 
+                ) as T1 
+                FULL JOIN
+                (SELECT Catalog, CardName, COUNT(*) as FailedTests
+                From Logs
+                WHERE Catalog=@Catalog AND Logs.Date between @StartDate and @EndDate AND FinalResult = 'FAIL'
+                GROUP BY Catalog, CardName
+                ) as T2
+	                ON T1.CardName = T2.CardName
                 ) ";
                 var objects = await connection.QueryAsync<dynamic>(sqlCommand,
                     new { Catalog = cardYield.Catalog, StartDate = cardYield.StartDate, EndDate = cardYield.EndDate });
+
+                /*if (objects.Count() == 0)
+                {
+                    return new Result<List<dynamic>>(true, new List<dynamic>(), "No data was found");
+                }
+                */
                 return new Result<List<dynamic>>(true, objects.AsList());
             }
             catch (Exception e)
