fa6ebdb8ad5cf30bdc87bccfb01804d2672438aa
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2021-12-30 18:50:24 +0200

    Add some unit tests for fileManager

---
+++ ETLTests/FileManagerTests.cs
@@ -0,0 +1,53 @@
+﻿using Xunit;
+using ETL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ETLTests;
+
+namespace ETL.Tests
+{
+    public class FileManagerTests : TestSuit<FileManager>
+    {
+
+
+        /// <summary>
+        /// Verify that all the files which been return are json files
+        /// </summary>
+        [Fact()]
+        public void GetFilesTests_JsonFormat()
+        {
+            var fileManager = GetConfiguratedComponent("Valid");
+            var filesResult = fileManager.GetNewLogs();
+            Assert.True(filesResult.Status);
+            foreach (var file in filesResult.Data)
+            {
+                Assert.EndsWith(".json", file);
+            }     
+
+        }
+
+        /// <summary>
+        /// Verify searching for new logs is in all the subdirectories
+        /// </summary>
+        [Fact()]
+        public void GetFilesTests_EmptyParentDirectory()
+        {
+            var fileManager = GetConfiguratedComponent("Valid");
+            var filesResult = fileManager.GetNewLogs();
+            Assert.True(filesResult.Status);
+            Assert.NotEmpty(filesResult.Data);
+        }
+
+        /// <summary>
+        /// Verify searching for new logs is in all the subdirectories
+        /// </summary>
+        [Fact()]
+        public void GetFilesTests_DirectoryNotFound()
+        {
+            var fileManager = GetConfiguratedComponent("NonExisitngDirectory");
+            var filesResult = fileManager.GetNewLogs();
+            Assert.False(filesResult.Status);
+        }
+    }
+}
---
+++ ETLTests/TestSuit.cs
@@ -0,0 +1,40 @@
+﻿using ETL;
+using ETL.Repository;
+using ETL.Utils;
+using ETL.Utils.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETLTests
+{
+    public class TestSuit<T>
+    {
+        public T GetConfiguratedComponent(string profile)
+        {
+            var servicesProvider = ConfigureServices(profile);
+            return servicesProvider.GetService<T>();
+        }
+
+        /// <summary>
+        /// Utility function to build service provider for dependency injection
+        /// </summary>
+        /// <returns></returns>
+        private  ServiceProvider ConfigureServices(string profile)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").AddEnvironmentVariablesForTesting(profile).Build();
+            var serviceProvier = new ServiceCollection()
+                .Configure<Directories>(config.GetSection("Directories"))
+                .Configure<DatabaseSettings>(config.GetSection("DatabaseSettings"))
+
+                .AddSingleton<ILogger>(sp => new Logger("ETL-process"))
+                .AddSingleton<FileManager>()
+                .AddSingleton<Deserializer>()
+                .AddSingleton<Repository>()
+                .BuildServiceProvider();
+            return serviceProvier;
+        }
+    }
+}
