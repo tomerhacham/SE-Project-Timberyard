580c7bcb6a8848acad487b0ec5117f7c164284a5
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2022-03-09 17:44:14 +0200

    returns input validation in business for test purposes

--- WebService/Domain/Business/Queries/QueriesController.cs
+++ WebService/Domain/Business/Queries/QueriesController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using WebService.Domain.DataAccess;
 using WebService.Utils;
@@ -28,20 +29,52 @@ namespace WebService.Domain.Business.Queries
 
         public async Task<Result<QueryResult>> CalculateCardYield(string catalog, DateTime startDate, DateTime endDate)
         {
+            Result<QueryResult> inputValidation = IsValidInputs(startDate, endDate,catalog: catalog);
+            if (!inputValidation.Status)
+            {
+                return inputValidation;
+            }
             var query = new CardYield(catalog, startDate, endDate);
             return await query.Execute(LogsAndTestsRepository);
         }
 
         public async Task<Result<QueryResult>> CalculateStationsYield(DateTime startDate, DateTime endDate)
         {
+            Result<QueryResult> inputValidation = IsValidInputs(startDate, endDate);
+            if (!inputValidation.Status)
+            {
+                return inputValidation;
+            }
             var query = new StationsYield(startDate, endDate);
             return await query.Execute(LogsAndTestsRepository);
         }
         public async Task<Result<QueryResult>> CalculateStationAndCardYield(string station, string catalog, DateTime startDate, DateTime endDate)
         {
+            Result<QueryResult> inputValidation = IsValidInputs(startDate, endDate,catalog:catalog, station:station);
+            if (!inputValidation.Status)
+            {
+                return inputValidation;
+            }
             var query = new StationAndCardYield(station, catalog, startDate, endDate);
             return await query.Execute(LogsAndTestsRepository);
         }
 
+        private Result<QueryResult> IsValidInputs(DateTime startDate, DateTime endDate, [Optional] string catalog, [Optional] string station)
+        {
+            if (catalog != null && catalog == "")
+            {
+                return new Result<QueryResult>(false, null, "Invalid catalog name\n");
+            }
+            if (station != null && station == "")
+            {
+                return new Result<QueryResult>(false, null, "Invalid station name\n");
+            }
+            if (startDate > endDate)
+            {
+                return new Result<QueryResult>(false, null, "Invalid range of dates\n");
+            }
+            return new Result<QueryResult>(true, null, "All inputs are valid\n");
+
+        }
     }
 }
