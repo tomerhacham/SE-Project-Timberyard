272afb21cca3c37dda753673c4e1ddbf1f261ac1
shakedshmar <shakedshmar@gmail.com>
2022-04-20 16:06:43 +0300

    update station yeild tests

--- Timberyard-UnitTests/IntegrationTests/StationsYieldTests.cs
+++ Timberyard-UnitTests/IntegrationTests/StationsYieldTests.cs
@@ -19,11 +19,11 @@ namespace Timberyard_UnitTests.IntegrationTests
         }
 
         [Theory]
-        [InlineData(2021, 2021, true, new string[] { "04", "11", "1P", "1T", "2L", "7S", "8D", "B2", "C2", "L4" }, new double[] { 100, 91.666666666666, 0, 94.736842105263, 0, 0, 100, 33.333333333333, 100, 60 })]
-        [InlineData(2021, 2020, false, new string[] { }, new double[] { })]
+        [InlineData(2021, 2022, true, new string[] { "11", "2T", "2X", "L4", "L5" }, new double[] { 60, 0, 50, 50, 70 })]                                       // Happy 
+        [InlineData(2022, 2021, false, new string[] { }, new double[] { })]                                                                                     // Bad: Invalid dates
         public async void StationsYield_Scenarios_Test(int startDate, int endDate, bool expectedResult, string[] stationNames, double[] SuccessRatioValues)
         {
-            Result<QueryResult> queryResult = await QueriesController.CalculateStationsYield(new DateTime(startDate, 01, 01), new DateTime(endDate, 01, 03));
+            Result<QueryResult> queryResult = await QueriesController.CalculateStationsYield(new DateTime(startDate, 12, 01), new DateTime(endDate, 12, 01));
             Assert.Equal(expectedResult, queryResult.Status);
             if (expectedResult)
             {
