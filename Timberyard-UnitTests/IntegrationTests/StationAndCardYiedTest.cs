d3b016f9a044568ea442eb1be754e7e4469db1df
shakedshmar <shakedshmar@gmail.com>
2022-04-20 16:34:17 +0300

    update station and card yield tests

--- Timberyard-UnitTests/IntegrationTests/StationAndCardYiedTest.cs
+++ Timberyard-UnitTests/IntegrationTests/StationAndCardYiedTest.cs
@@ -19,23 +19,25 @@ namespace Timberyard_UnitTests.IntegrationTests
         }
 
         [Theory]
-        [InlineData("10B", "X16434", 2020, 2020, true, new string[] { "X16434" }, new string[] { "XFCMV14" }, new double[] { 54.545454545454 })]          // Happy : There are X records of the inputs out of Y records
-        [InlineData("10B", "X16434", 2020, 2019, false, new string[] { "X16434" }, new string[] { "XFCMV14" }, new double[] { 54.545454545454 })]         // Bad: invalid dates
-        [InlineData("10B", "", 2020, 2020, false, new string[] { "X16434" }, new string[] { "XFCMV14" }, new double[] { 54.545454545454 })]               // Bad: empty catalog
-        [InlineData("", "X16434", 2020, 2020, false, new string[] { "X16434" }, new string[] { "XFCMV14" }, new double[] { 54.545454545454 })]            // Bad: empty station
+        [InlineData("L5", "X93655", 2021, 2022, true, new string[] { "9901X_JTAG" }, new double[] { 70 })]                          // Happy : There are X records of the inputs out of Y records
+        [InlineData("11", "X39337", 2021, 2022, true, new string[] { "RCP08_O_JTAG" }, new double[] { 80 })]                        // Happy : There are X records of the inputs out of Y records
+        [InlineData("L", "X93655", 2021, 2022, true, new string[] { }, new double[] { })]                                           // Happy : There are no records since station name does not exsists
+        [InlineData("L5", "X93677", 2021, 2022, true, new string[] { }, new double[] { })]                                          // Happy : There are no records since catalog does not exsists
+        [InlineData("L5", "X93655", 2017, 2018, true, new string[] { }, new double[] { })]                                          // Happy : There are no records since catalog does not exsists
+        [InlineData("10B", "X16434", 2020, 2019, false, new string[] { }, new double[] { })]              // Bad: invalid dates
+        [InlineData("10B", "____", 2021, 2022, false, new string[] { }, new double[] { })]                // Bad: invalid catalog
+        [InlineData("____", "X16434", 2021, 2022, false, new string[] { }, new double[] { })]             // Bad: invalid station
 
-        public async void StationAndCardYield_Scenarios_Test(string station, string catalog, int startDate, int endDate, bool expectedResult, string[] catalogNames, string[] cardNames, double[] SuccessRatioValues)
+        public async void StationAndCardYield_Scenarios_Test(string station, string catalog, int startDate, int endDate, bool expectedResult, string[] cardNames, double[] SuccessRatioValues)
         {
-            Result<QueryResult> queryResult = await QueriesController.CalculateStationAndCardYield(station, catalog, new DateTime(startDate, 11, 15), new DateTime(endDate, 11, 30));
+            Result<QueryResult> queryResult = await QueriesController.CalculateStationAndCardYield(station, catalog, new DateTime(startDate, 12, 01), new DateTime(endDate, 12, 01));
             Assert.Equal(expectedResult, queryResult.Status);
             if (expectedResult)
             {
                 Assert.Equal(new string[] { "Catalog", "CardName", "SuccessRatio" }, queryResult.Data.ColumnNames);
-                Assert.Equal(catalogNames.Length, queryResult.Data.Records.Count);
                 var data = queryResult.Data;
-                for (int i = 0; i < catalogNames.Length; i++)
+                for (int i = 0; i < cardNames.Length; i++)
                 {
-                    Assert.Equal(catalogNames[i], data.Records[i].Catalog);
                     Assert.Equal(cardNames[i], data.Records[i].CardName);
                     Assert.Equal(SuccessRatioValues[i], data.Records[i].SuccessRatio);
                     Assert.True(0 <= data.Records[i].SuccessRatio && data.Records[i].SuccessRatio <= 100);
