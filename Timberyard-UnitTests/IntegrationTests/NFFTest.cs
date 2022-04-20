3303f339bc18c23124369b04344eb46fb0ad252b
shakedshmar <shakedshmar@gmail.com>
2022-04-20 17:03:34 +0300

    update NFF tests

--- Timberyard-UnitTests/IntegrationTests/NFFTest.cs
+++ Timberyard-UnitTests/IntegrationTests/NFFTest.cs
@@ -19,17 +19,33 @@ namespace Timberyard_UnitTests.IntegrationTests
         }
 
         [Theory]
-        [InlineData("XMCP-B", 2020, 2020, true, 2, "2X", "71489", new string[] {   "NO INF ALARMS CHECK (INF A&B ON) - PC14",
-                                                                                "NO INF ALARMS CHECK (INF A&B ON)-ALTERA",
-                                                                                "INF A INPUT",
-                                                                                "INF B INPUT ALARM CHECK (INF A ON , B OFF)- PC14",
-                                                                                "INF B INPUT ALARM CHECK (INF A ON , B OFF)-ALTERA",
-                                                                                "INF B INPUT",
-                                                                                "INF A INPUT ALARM CHECK (INF A OFF , B ON)- PC14",
-                                                                                "INF A INPUT ALARM CHECK (INF A OFF , B ON)-ALTERA",
-                                                                                "MCP FLASH BOOT TEST",
-                                                                                "NVM test" })]
-        [InlineData("", 2020, 2020, false, 0, "2X", "71489", new string[] { "NO INF ALARMS CHECK (INF A&B ON) - PC14" })]
+        [InlineData("X93655", 2021, 2022, true, 54 , "L5", "75653", new string[] {   "CAPTURE",
+                                                                                "IDENT",
+                                                                                "Program_SFL3710",
+                                                                                "QSPI Programming",
+                                                                                "Program_SFL3710",
+                                                                                "SHUTDOWN",
+                                                                                "inter_U25_U54_U60_U61",
+                                                                                "inter_U60_U120",
+                                                                                "inter_U61_U120",
+                                                                                "inter_U153_U120",
+                                                                                "COMPARE UUT_CN",
+                                                                                "COMPARE UUT OPTION",
+                                                                                "COMPARE UUT_REV_VER",
+                                                                                "Init Dynamic-File-Creator",
+                                                                                "Create Dynamic-File-Creator",
+                                                                                "Compile Bin File",
+                                                                                "Program_ICL3710",
+                                                                                "QSPI Programming",
+                                                                                "inter_U25_U54_U60_U61",
+                                                                                "inter_U60_U120",
+                                                                                "Init Dynamic-File-Creator",
+                                                                                "Create Dynamic-File-Creator"
+                                                                                })]
+        [InlineData("X93655", 2017, 2018, false, 0, "", "", new string[] { })]                                                                         // Happy: no records since no data between dates
+        [InlineData("X93677", 2021, 2022, false, 0, "", "", new string[] { })]                                                                         // Happy: no records since no catalog
+        [InlineData("X93655", 2022, 2021, false, 0, "", "", new string[] { })]                                                                         // Bad: Invalid dates
+        [InlineData("___", 2021, 2022, false, 0, "", "", new string[] { })]                                                                            // Bad: Invalid catalog
         public async void NFF_Scenarios_Test(string cardName, int startDate, int endDate, bool expectedResult, int expectedNumOfRecords, string stationNames, string operators, string[] failedTestNames)
         {
             Result<QueryResult> queryResult = await QueriesController.CalculateNFF(cardName, new DateTime(startDate, 11, 30), new DateTime(endDate, 11, 30), 5* 60 * 60);
