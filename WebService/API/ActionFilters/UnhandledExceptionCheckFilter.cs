bedbb5fefe921e7348224d012e39da858deb1494
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2022-04-26 12:08:57 +0300

    Alarms (#64)
    * add alarm repository functionality
    
    Add alarm controller
    add alarm DTO
    add alarm model
    add ISMTPclient interface
    
    * Add model validation for alarms receivers
    
    * Stump for alarm query
    
    * the method check condition for alarm added
    
    * add implementation for check alarm condition
    
    * add objective to all signatures
    
    * updated the alarm notification message
    
    * add SMTPClient stub
    
    * add alarm and user repository to startup DI
    
    * refactor
    
    * apply formatter and codeCleanUp
    
    * finish implement alarms
    
    * add tests and remove redundant code
    
    * add SMTP support with mock server
    
    * change result to false when catch exception
    
    * Add alarms unit test
    
    * Add alarms unit tests
    
    * refactor smtp client
    
    * apply formatter
    
    * fix missing type
    
    Co-authored-by: Zoe-Ellins <zoee@post.bgu.ac.il>
    Co-authored-by: shakedshmar <shakedshmar@gmail.com>

--- WebService/API/ActionFilters/UnhandledExceptionCheckFilter.cs
+++ WebService/API/ActionFilters/UnhandledExceptionCheckFilter.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using WebService.API.Controllers.Models;
 using WebService.Utils;
 
