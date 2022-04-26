e8bc8c2f7c8ffc1e7a2042e017c17c83534e0432
shakedshmar <shakedshmar@gmail.com>
2022-04-26 11:12:33 +0300

    Card Test Duration tests run and fix

--- Timberyard-UnitTests/IntegrationTests/CardTestDurationTests.cs
+++ Timberyard-UnitTests/IntegrationTests/CardTestDurationTests.cs
@@ -19,26 +19,31 @@ namespace Timberyard_UnitTests.IntegrationTests
             QueriesController = serviceProvider.GetService<QueriesController>();
         }
 
-        // TODO - change inputs according to db
         [Theory]
         [InlineData("X93655", 2021, 2022, true, 2, new string[] { "75653", "73003" }, new double[] { 1085, 2052 }, new double[] { 1086, 2053 })]                                          // Happy : There are X records of the inputs out of Y records ( where X==Y )
         [InlineData("X17837", 2021, 2022, true, 1, new string[] { "71220" }, new double[] { 814 }, new double[] { 815 })]                                                                 // Happy : There are X records of the inputs out of Y records ( where X<Y )
         [InlineData("X93677", 2021, 2022, true, 0, new string[] {}, new double[] {}, new double[] {})]                                                                                    // Happy : There are 0 records of the inputs out of Y records  ( catalog does not exists )      
-        [InlineData("___", 2021, 2011, true, 0, new string[] { }, new double[] { }, new double[] { })]                                                                                    // Bad : Invalid catalog           
-        [InlineData("X16434", 2022, 2021, true, 0, new string[] {}, new double[] {}, new double[] {})]                                                                                    // Bad : invalid dates         
+        [InlineData("___", 2021, 2011, false, 0, new string[] { }, new double[] { }, new double[] { })]                                                                                    // Bad : Invalid catalog           
+        [InlineData("X16434", 2022, 2021, false, 0, new string[] {}, new double[] {}, new double[] {})]                                                                                    // Bad : invalid dates         
         public async void CardTestDuration_Scenarios_Test
             (string catalog, int startDate, int endDate, bool query_result, int records_count, string[] Operators_results, double[] NetTimeAvg_results, double[] TotalTimeAvg_results)
         {
             Result<QueryResult> queryResult = await QueriesController.CalculateCardTestDuration(catalog, new DateTime(startDate, 11, 11), new DateTime(endDate, 11, 12));
             Assert.Equal(query_result, queryResult.Status);
-            Assert.Equal(new string[] { "Operator", "NetTimeAvg", "TotalTimeAvg" }, queryResult.Data.ColumnNames);
-            Assert.Equal(records_count, queryResult.Data.Records.Count);
+            if (queryResult.Status)
+            {
+                if (queryResult.Data.ColumnNames.Length > 0)
+                {
+                    Assert.Equal(new string[] { "Operator", "NetTimeAvg", "TotalTimeAvg" }, queryResult.Data.ColumnNames);
+                }
+                Assert.Equal(records_count, queryResult.Data.Records.Count);
+            }
 
             for (int i = 0; i < records_count; i++)
             {
-                Assert.Equal(Operators_results[i], queryResult.Data.Records[i]["Operator"]);
-                Assert.Equal(NetTimeAvg_results[i], queryResult.Data.Records[i]["NetTimeAvg"]);
-                Assert.Equal(TotalTimeAvg_results[i], queryResult.Data.Records[i]["TotalTimeAvg"]);
+                Assert.Equal(Operators_results[i], queryResult.Data.Records[i].Operator);
+                Assert.Equal(NetTimeAvg_results[i], queryResult.Data.Records[i].NetTimeAvg);
+                Assert.Equal(TotalTimeAvg_results[i], queryResult.Data.Records[i].TotalTimeAvg);
             }
 
         }
