ccc7730bc484df2d3e38b74ad5235c74d72104c6
shakedshmar <shakedshmar@gmail.com>
2022-04-26 11:59:54 +0300

    NFF tests run and fix

--- Timberyard-UnitTests/IntegrationTests/NFFTest.cs
+++ Timberyard-UnitTests/IntegrationTests/NFFTest.cs
@@ -19,7 +19,7 @@ namespace Timberyard_UnitTests.IntegrationTests
         }
 
         [Theory]
-        [InlineData("X93655", 2021, 2022, true, 54 , "L5", "75653", new string[] {   "CAPTURE",
+        [InlineData("9901X_JTAG", 2021, 2022, true, 2 , new string[] { "L5" , "L5" }, new string[] { "75653", "73003" }, new string[] {   "CAPTURE",
                                                                                 "IDENT",
                                                                                 "Program_SFL3710",
                                                                                 "QSPI Programming",
@@ -42,26 +42,28 @@ namespace Timberyard_UnitTests.IntegrationTests
                                                                                 "Init Dynamic-File-Creator",
                                                                                 "Create Dynamic-File-Creator"
                                                                                 })]
-        [InlineData("X93655", 2017, 2018, false, 0, "", "", new string[] { })]                                                                         // Happy: no records since no data between dates
-        [InlineData("X93677", 2021, 2022, false, 0, "", "", new string[] { })]                                                                         // Happy: no records since no catalog
-        [InlineData("X93655", 2022, 2021, false, 0, "", "", new string[] { })]                                                                         // Bad: Invalid dates
-        [InlineData("___", 2021, 2022, false, 0, "", "", new string[] { })]                                                                            // Bad: Invalid catalog
-        public async void NFF_Scenarios_Test(string cardName, int startDate, int endDate, bool expectedResult, int expectedNumOfRecords, string stationNames, string operators, string[] failedTestNames)
+        [InlineData("9901X_JTAG", 2017, 2018, true, 0, new string[] { }, new string[] { }, new string[] { })]                                                                         // Happy: no records since no data between dates
+        [InlineData("X93677", 2021, 2022, true, 0, new string[] { }, new string[] { }, new string[] { })]                                                                         // Happy: no records since no catalog
+        [InlineData("X93655", 2022, 2021, false, 0, new string[] { }, new string[] { }, new string[] { })]                                                                         // Bad: Invalid dates
+        [InlineData("", 2021, 2022, false, 0, new string[] { }, new string[] { }, new string[] { })]                                                                            // Bad: Invalid catalog
+        public async void NFF_Scenarios_Test(string cardName, int startDate, int endDate, bool expectedResult, int expectedNumOfRecords, string[] stationNames, string[] operators, string[] failedTestNames)
         {
             Result<QueryResult> queryResult = await QueriesController.CalculateNFF(cardName, new DateTime(startDate, 12, 01), new DateTime(endDate, 12, 01), 5* 60 * 60);
             Assert.Equal(expectedResult, queryResult.Status);
             if (expectedResult)
             {
-                Assert.Equal(new string[] { "Date", "CardName", "Catalog", "Station", "Operator", "FailedTests" }, queryResult.Data.ColumnNames);
-                Assert.Equal(expectedNumOfRecords, queryResult.Data.Records.Count);
-                var data = queryResult.Data;
-                foreach (var record in data.Records)
+                if (queryResult.Data.ColumnNames.Length > 0)
                 {
-                    Assert.Equal(stationNames, record.Station);
-                    Assert.Equal(operators, record.Operator);
+                    Assert.Equal(new string[] { "Date", "CardName", "Catalog", "Station", "Operator", "FailedTests" }, queryResult.Data.ColumnNames);
+                }
+                Assert.Equal(expectedNumOfRecords, queryResult.Data.Records.Count);
+                for (int i = 0; i < expectedNumOfRecords; i++)
+                {
+                    Assert.Equal(stationNames[i], queryResult.Data.Records[i].Station);
+                    Assert.Equal(operators[i], queryResult.Data.Records[i].Operator);
                     foreach (var testName in failedTestNames)
                     {
-                        Assert.Contains(testName, record.FailedTests);
+                        Assert.Contains(testName, queryResult.Data.Records[i].FailedTests);
                     }
                 }
             }
