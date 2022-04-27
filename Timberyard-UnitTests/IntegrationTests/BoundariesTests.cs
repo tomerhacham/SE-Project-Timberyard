1a01b0cb15a85f270007a5a95a8c2423c3d364a3
Zoe-Ellins <zoee@post.bgu.ac.il>
2022-04-27 10:52:27 +0300

    boundaries integration tests

--- Timberyard-UnitTests/IntegrationTests/BoundariesTests.cs
+++ Timberyard-UnitTests/IntegrationTests/BoundariesTests.cs
@@ -21,16 +21,18 @@ namespace Timberyard_UnitTests.IntegrationTests
 
 
         [Theory]
-        [InlineData("10B", 2020, 2020, true, new string[] { "X16434" }, new string[] { "XFCMV14" }, new double[] { 54.545454545454 })]          // Happy : There are X records of the inputs out of Y records
-        [InlineData("10B", 2020, 2019, false, new string[] { "X16434" }, new string[] { "XFCMV14" }, new double[] { 54.545454545454 })]         // Bad: invalid dates
-        [InlineData("",2020, 2020, false, new string[] { "X16434" }, new string[] { "XFCMV14" }, new double[] { 54.545454545454 })]          // Bad: empty catalog                                                                                                                          
+        [InlineData("X39337", 2021, 2022, true, new string[] { "Boundaries_Test2", "Boundaries_Test1" }, new double[] { 2, 1 }, new double[] { 6, 4 }, new double[] { 4, 2.24 }, new double[] {1.41421, 0.749577})]          // Happy : There are X records of the inputs out of Y records
+        [InlineData("X39337", 2020, 2019, false, new string[] {}, new double[] {}, new double[] {}, new double[] {}, new double[] {})]                  // Bad: invalid dates
+        [InlineData("",2020, 2020, false, new string[] {}, new double[] {}, new double[] {}, new double[] {}, new double[] {})]                         // Bad: empty catalog
+        [InlineData("X12345", 2020, 2020, false, new string[] { }, new double[] { }, new double[] { }, new double[] { }, new double[] { })]             // Bad: catalog                                                                                                                          
+
         public async void Boundaries_Scenarios_Test(string catalog, int startDate, int endDate, bool expectedResult, String[] testNames, double[] minValue, double[] maxValue, double[] average, double[] standardDeviation)
         {
-            Result<QueryResult> queryResult = await QueriesController.CalculateBoundaries(catalog, new DateTime(startDate, 11, 15), new DateTime(endDate, 11, 30));
+            Result<QueryResult> queryResult = await QueriesController.CalculateBoundaries(catalog, new DateTime(startDate, 11, 15), new DateTime(endDate, 01, 22));
             Assert.Equal(expectedResult, queryResult.Status);
             if (expectedResult)
             {
-                Assert.Equal(new string[] { "TestName", "Min", "Max", "Average", "StandardDeviation" }, queryResult.Data.ColumnNames);
+                Assert.Equal(new string[] { "TestName", "Min", "Max", "Average", "StandardDeviation", "Received" }, queryResult.Data.ColumnNames);
                 Assert.Equal(testNames.Length, queryResult.Data.Records.Count);
                 var data = queryResult.Data;
                 for (int i = 0; i < testNames.Length; i++)
@@ -38,8 +40,8 @@ namespace Timberyard_UnitTests.IntegrationTests
                     Assert.Equal(testNames[i], data.Records[i].TestName);
                     Assert.Equal(minValue[i], data.Records[i].Min);
                     Assert.Equal(maxValue[i], data.Records[i].Max);
-                    Assert.Equal(average[i], data.Records[i].Average);
-                    Assert.Equal(standardDeviation[i], data.Records[i].StandardDeviation);
+                    Assert.Equal(average[i], data.Records[i].Average, 2);
+                    Assert.Equal(standardDeviation[i], data.Records[i].StandardDeviation, 5);
                 }
             }
         }
