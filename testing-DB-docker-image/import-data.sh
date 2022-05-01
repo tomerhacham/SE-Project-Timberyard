f6c22e8da064449fb0c96cf1a36ae1fb3e6d1c66
shakedshmar <shakedshmar@gmail.com>
2022-05-01 14:25:58 +0300

    add Data file

--- testing-DB-docker-image/import-data.sh
+++ testing-DB-docker-image/import-data.sh
@@ -4,5 +4,11 @@ sleep 90s
 #run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Password1!" -i setup.sql
 
+# wait for scheme to build
+sleep 90s
+
+#run the setup script to populate DB with data
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Password1!" -i dataScript.sql
+
 # import the data from the csv file
 #/opt/mssql-tools/bin/bcp heroes.dbo.HeroValue in "/usr/work/heroes.csv" -c -t',' -S localhost -U SA -P "Password1!" -d heroes
