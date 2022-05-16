95c7e93d50ffb8d9605b01efe6f83d62fa0dba6a
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2022-05-16 16:00:27 +0300

    Add default system admin

---
+++ WebService/Utils/Models/DefaultSystemAdmin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebService.Utils.Models
+{
+    public class DefaultSystemAdmin
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
