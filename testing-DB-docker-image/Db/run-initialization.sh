61456b91bd48831694b4618a62e65ebfcd2d629e
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2022-05-19 17:23:10 +0300

    try different port

--- testing-DB-docker-image/Db/run-initialization.sh
+++ testing-DB-docker-image/Db/run-initialization.sh
@@ -1,6 +1,8 @@
 # Wait to be sure that SQL Server came up
-sleep 90s
+echo 'Sleeping for 90 sec'
+sleep 90
+echo 'Start building scheme and inserting data'
 
 # Run the setup script to create the DB and the schema in the DB
 # Note: make sure that your password matches what is in the Dockerfile
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P password123! -d master -i setup.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password123! -d master -i setup.sql
